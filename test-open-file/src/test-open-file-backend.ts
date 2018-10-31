7a0e8ffd3ef1891896a3fb8cb85d81ea4165c519
Sun Tan <sutan@redhat.com>
2018-10-31 16:12:35 +0000

    setting back open-file id
    Signed-off-by: Sun Tan <sutan@redhat.com>

--- test-open-file/src/test-open-file-backend.ts
+++ test-open-file/src/test-open-file-backend.ts
@@ -12,7 +12,7 @@ export function start(context: theia.PluginContext) {
     };
     context.subscriptions.push(theia.commands.registerCommand(informationMessageTestCommand, (...args: any[]) => {
         theia.window.showInformationMessage('Hello World!');
-        theia.commands.executeCommand('file-search:openFile', 'file:///projects/theia/README.md')
+        theia.commands.executeCommand('file-search.openFile', 'file:///projects/theia/README.md')
             .then(() => {
                 theia.window.showInformationMessage('open file looks ok');
             }, e => {
